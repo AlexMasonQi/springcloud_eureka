71a29c729d9675845ba11e179b47d609064a8dbc
alex-java <americaf35@Outlook.com>
2018-07-04 23:43:56 +0800

    zuul actuator

--- discovery-zuul-proxy/src/main/java/com/springcloud/ZuulApplication.java
+++ discovery-zuul-proxy/src/main/java/com/springcloud/ZuulApplication.java
@@ -8,6 +8,20 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ZuulApplication
 {
+    //open the hystrix dashboard(Spring Boot Version 2.0+)
+//    @Bean
+//    public ServletRegistrationBean getServlet()
+//    {
+//        ZuulServlet zuulServlet = new ZuulServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(zuulServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/actuator/routes");
+//        registrationBean.addUrlMappings("/actuator/routes/details");
+//        registrationBean.setName("ZuulServlet");
+//
+//        return registrationBean;
+//    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(ZuulApplication.class, args);
